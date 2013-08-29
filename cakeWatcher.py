a1a09d504ee109ac1364f31a37959f0b9de4aa4a
thomasmacpherson <thomasmacpherson@live.co.uk>
2013-08-29 09:31:39 +0000

    removed espeak module error prints by calling in speak in shell command and sending error to /dev/null

--- cakeWatcher.py
+++ cakeWatcher.py
@@ -2,7 +2,9 @@ from espeak import espeak
 import pifacedigitalio as piface
 piface.init()
 
-statements = ("Close this door now", "You will regret it tomorrow","A moment on the lips, you know the rest")
+import os
+
+statements = ("Close this door now", "You will regret it tomorrow","A moment on the lips you know the rest")
 numberOfStatements = len(statements)
 currentStatement = 0
 
@@ -11,8 +13,11 @@ inputPin = 0
 
 while True:
 	if piface.digital_read(inputPin):
-		espeak.synth(statements[currentStatement%numberOfStatements])
-		print statements[currentStatement%numberOfStatements]
+		statement = statements[currentStatement%numberOfStatements]
+		print statement
+		speakCall = "espeak '" + statement + "' 2>/dev/null"
+		os.system(speakCall)
 		currentStatement+=1
+
 		while piface.digital_read(inputPin):
 			pass
